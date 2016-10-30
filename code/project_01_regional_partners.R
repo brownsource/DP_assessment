ead0c28d9646ed7c87c52e4e40a48bbe59feae5b
Harry <harry@brown-source.com>
2016-10-30 15:48:38 +0200

    Regional partner script working

--- code/project_01_regional_partners.R
+++ code/project_01_regional_partners.R
@@ -29,10 +29,9 @@ library("ForceAtlas2")
 graph_edges <- data.frame(read.csv("../not_shared/data/regional_partners_edges.csv", header = TRUE, stringsAsFactors = FALSE))
 graph_nodes <- data.frame(read.csv("../not_shared/data/regional_partners_nodes.csv", header = TRUE, stringsAsFactors = FALSE))
 
+
 #Create an igraph object
 network_all <- graph_from_data_frame(d=graph_edges, vertices = graph_nodes, directed = F)
-E(network_all)$from <- graph_edges$from
-E(network_all)$to <- graph_edges$to
 
 #SET THE LAYOUT PARAMETERS
 layout1 <- layout.forceatlas2(network_all, 
@@ -41,7 +40,7 @@ layout1 <- layout.forceatlas2(network_all,
                               linlog = FALSE,
                               pos = NULL, 
                               nohubs = FALSE, 
-                              k = 400, 
+                              k = 200, 
                               gravity = 0.5, 
                               ks = 0.1, 
                               ksmax = 10, 
@@ -49,8 +48,15 @@ layout1 <- layout.forceatlas2(network_all,
                               center = NULL, 
                               #tolerate = 0.1, 
                               dim = 2, 
-                              plotstep = 100, 
+                              plotstep = 500, 
                               plotlabels = FALSE) 
+
+#If you need to re-run the force atlas to create a more pleaseing rotation
+#For some reason either these two lines will stop the code above working OR
+#It won't work if you create a plot. 
+#The temporary solution is to remove all items from the environment and re-run the script
+E(network_all)$from <- graph_edges$from
+E(network_all)$to <- graph_edges$to
 
 #SET THE PLOT PARAMETERS
 #Assign colours to vertexes based on type
@@ -58,6 +64,8 @@ V(network_all)[V(network_all)$type == "Country"]$color <- "#009460"
 V(network_all)[V(network_all)$type == "UN Agency"]$color <- "#0072B6"
 V(network_all)[V(network_all)$type == "Government"]$color <- "#FF5300"
 V(network_all)[V(network_all)$type == "INGO"]$color <- "#FF8C00"
+V(network_all)[V(network_all)$type == "Local"]$color <- "#FFCC00"
+V(network_all)[V(network_all)$type == "Other"]$color <- "grey80"
 V(network_all)[V(network_all)$type == "Not yet classified"]$color <- "grey80"
 #TEST V(network_all)$color
 
@@ -82,21 +90,56 @@ E(network_all)$margin <- 1
 #OUTPUTTING THE CHARTS
 #Initialising GhostScript... you will need to download this
 #and djust the path to match your installation of Ghostscript
-#Sys.setenv(R_GSCMD = "C:/Program Files/gs/gs9.20/bin/gswin64c.exe")
-
-#pdf("../not_shared/output/regional_partners1.pdf", family="Arial", width=4, height=4)
-windows(width=8, height=8)
+Sys.setenv(R_GSCMD = "C:/Program Files/gs/gs9.20/bin/gswin64c.exe")
+png(file="../not_shared/output/regional_partners_hires.png",width = 10, height = 8, units = 'in', res = 300,bg = "transparent")
+par(mar=c(5,3,2,2)+0.1)
 plot(network_all, layout = layout1)
-
-legend(x="bottomleft", 
+legend(x="bottom", 
        title.adj = 0,
-       c("Country","UN Agency", "Government", "INGO", "Not yet classified"), 
-       pt.bg=c("#009460","#0072B6","#FF5300","#FF8C00","grey80"),
+       c("Country","UN Agency", "Government", "INGO", "Local", "Other","Not yet classified"), 
+       pt.bg=c("#009460","#0072B6","#FF5300","#FF8C00","#FFCC00","grey80", "grey80"),
        pch=21,
        col=NA, 
        pt.cex=1.5, 
        cex=0.75, 
        bty="n", 
        horiz=TRUE)
-#dev.off()
-#embed_fonts("../not_shared/output/regional_partners1.pdf", outfile="../not_shared/output/regional_partners1.pdf")
+dev.off()
+
+
+pdf(file="../not_shared/output/regional_partners_hires.pdf",width = 10, height = 8)
+par(mar=c(5,3,2,2)+0.1)
+plot(network_all, layout = layout1)
+legend(x="bottom", 
+       title.adj = 0,
+       c("Country","UN Agency", "Government", "INGO", "Local", "Other","Not yet classified"), 
+       pt.bg=c("#009460","#0072B6","#FF5300","#FF8C00","#FFCC00","grey80", "grey80"),
+       pch=21,
+       col=NA, 
+       pt.cex=1.5, 
+       cex=0.75, 
+       bty="n", 
+       horiz=TRUE)
+embed_fonts("../not_shared/output/regional_partners.pdf", outfile="../not_shared/output/regional_partners.pdf")
+dev.off()
+
+tiff(file="../not_shared/output/regional_partners_hires.tiff",width = 10, height = 8, pointsize = 1/300, units = 'in', res = 300)
+par(mar=c(5,3,2,2)+0.1)
+plot(network_all, layout = layout1)
+legend(x="bottom", 
+       title.adj = 0,
+       c("Country","UN Agency", "Government", "INGO", "Local", "Other","Not yet classified"), 
+       pt.bg=c("#009460","#0072B6","#FF5300","#FF8C00","#FFCC00","grey80", "grey80"),
+       pch=21,
+       col=NA, 
+       pt.cex=1.5, 
+       cex=0.75, 
+       bty="n", 
+       horiz=TRUE)
+dev.off()
+
+
+
+tiff("Plot4b.tiff", width = 4, height = 4, pointsize = 1/300, units = 'in', res = 300)
+plot(x, y) # Make plot
+dev.off()
